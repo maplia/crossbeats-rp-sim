0074f3faf710713c4c14f16c2fc30ffc1548ff53
maplia <sie@maplia.jp>
2017-05-21 21:05:51 +0900

    2017/05/21-4

--- comrank/app.rb
+++ comrank/app.rb
@@ -142,27 +142,6 @@ module CxbRank
           :user => user, :skill_set => skill_set,
           :edit => edit, :ignore_locked => skill_options[:ignore_locked], :fixed_title => fixed_title}
       end
-
-      def music_skill_edit_page(user, &block)
-        if params[:music_text_id]
-          session[:music_text_id] = params[:music_text_id]
-        end
-        if session[:music_text_id].blank?
-          haml :error, :layout => true, :locals => {:error_no => ERROR_MUSIC_IS_UNDECIDED}
-        elsif (music = Master::Music.find_by(:text_id => session[:music_text_id])).nil?
-          haml :error, :layout => true, :locals => {:error_no => ERROR_MUSIC_NOT_EXIST}
-        else
-          curr_skill = PlayData::MusicSkill.find_by_user_and_music(user, music)
-          temp_skill = PlayData::MusicSkill.find_by_user_and_music(user, music)
-          if params[:music_text_id]
-            session[underscore(CxbRank::PlayData::MusicSkill)] = nil
-          end
-          if session[underscore(CxbRank::PlayData::MusicSkill)].present?
-            temp_skill.update_by_params!(session[underscore(CxbRank::PlayData::MusicSkill)])
-          end
-          yield curr_skill, temp_skill
-        end
-      end
     end
 
     get '/googlee47e6c106efd57d5.html' do
@@ -349,92 +328,6 @@ module CxbRank
         page_last_modified PAGE_TEMPLATE_FILES[CLEAR_LIST_VIEW_URI], data_mtime
         haml :skill_chart, :layout => true, :locals => {
           :user => user, :skill_chart => skill_chart, :fixed_title => fixed_title}
-      end
-    end
-
-    get "#{SKILL_ITEM_EDIT_URI}/?:music_text_id?" do
-      private_page do |user|
-        music_skill_edit_page(user) do |curr_skill, temp_skill|
-          settings.views << SiteSettings.join_comrank_path('views/music_skill_edit')
-          fixed_title = "#{PAGE_TITLES[SKILL_ITEM_EDIT_URI]} [#{curr_skill.music.full_title}]"
-          haml :music_skill_edit, :layout => true, :locals => {
-            :user => user, :curr_skill => curr_skill, :temp_skill => temp_skill, :fixed_title => fixed_title}
-        end
-      end
-    end
-
-    post SKILL_ITEM_EDIT_URI do
-      session[underscore(CxbRank::PlayData::MusicSkill)] = Hash[params[underscore(CxbRank::PlayData::MusicSkill)]]
-      private_page do |user|
-        music_skill_edit_page(user) do |curr_skill, temp_skill|
-          settings.views << SiteSettings.join_comrank_path('views/music_skill_edit')
-          unless temp_skill.valid?
-            haml :error, :layout => true,
-              :locals => {:errors => temp_skill.errors, :back_uri => SiteSettings.join_site_base(request.path_info)}
-          else
-            temp_skill.calc!
-            method = (params[:update].present? ? 'put' : 'delete')
-            fixed_title = "#{PAGE_TITLES[SKILL_ITEM_EDIT_URI]} [#{curr_skill.music.full_title}]"
-            haml :music_skill_edit_conf, :layout => true, :locals => {
-              :user => user, :curr_skill => curr_skill, :temp_skill => temp_skill,
-              :fixed_title => fixed_title, :method => method}
-          end
-        end
-      end
-    end
-
-    put SKILL_ITEM_EDIT_URI do
-      private_page do |user|
-        if params[:y].present?
-          music_skill_edit_page(user) do |curr_skill, temp_skill|
-            begin
-              temp_skill.calc!
-              temp_skill.save!
-              if SiteSettings.cxb_mode? or !user.point_direct
-                skill_set = SkillSet.new(user)
-                skill_set.load!
-                user.point = skill_set.total_point
-                user.point_direct = false
-                user.point_updated_at = Time.now
-                user.save!
-              end
-              session[:music_text_id] = nil
-              session[underscore(CxbRank::PlayData::MusicSkill)] = nil
-              redirect SiteSettings.join_site_base(SKILL_LIST_EDIT_URI)
-            rescue
-               haml :error, :layout => true,
-                 :locals => {:error_no => ERROR_DATABASE_SAVE_FAILED, :back_uri => SiteSettings.join_site_base(request.path_info)}
-            end
-          end
-        else
-          redirect SiteSettings.join_site_base(SKILL_ITEM_EDIT_URI)
-        end
-      end
-    end
-
-    delete SKILL_ITEM_EDIT_URI do
-      private_page do |user|
-        if params['y'].present?
-          music_skill_edit_page(user) do |curr_skill, temp_skill|
-            begin
-              temp_skill.destroy
-              skill_set = SkillSet.new(user)
-              skill_set.load!
-              user.point = skill_set.total_point
-              user.point_direct = false
-              user.point_updated_at = Time.now
-              user.save!
-              session[:music_text_id] = nil
-              session[underscore(CxbRank::PlayData::MusicSkill)] = nil
-              redirect SiteSettings.join_site_base(SKILL_LIST_EDIT_URI)
-            rescue
-              haml :error, :layout => true,
-                :locals => {:error_no => ERROR_DATABASE_SAVE_FAILED, :back_uri => SiteSettings.join_site_base(request.path_info)}
-            end
-          end
-        else
-          redirect SiteSettings.join_site_base(SKILL_ITEM_EDIT_URI)
-        end
       end
     end
 
--- comrank/lib/cxbrank/playdata/app.rb
+++ comrank/lib/cxbrank/playdata/app.rb
@@ -1,5 +1,7 @@
 require 'cxbrank/const'
+require 'cxbrank/master/music'
 require 'cxbrank/master/course'
+require 'cxbrank/playdata/music_skill'
 require 'cxbrank/playdata/course_skill'
 
 module CxbRank
@@ -7,11 +9,32 @@ module CxbRank
     class << self
       def registered app
         app.helpers do
+          def music_skill_edit_page(user, &block)
+            if params[:text_id]
+              session[:text_id] = params[:text_id]
+            end
+            if session[:text_id].blank?
+              haml :error, :layout => true, :locals => {:error_no => ERROR_MUSIC_IS_UNDECIDED}
+            elsif (music = Master::Music.find_by(:text_id => session[:text_id])).nil?
+              haml :error, :layout => true, :locals => {:error_no => ERROR_MUSIC_NOT_EXIST}
+            else
+              curr_skill = MusicSkill.find_by_user_and_music(user, music)
+              temp_skill = MusicSkill.find_by_user_and_music(user, music)
+              if params[:text_id]
+                session[underscore(temp_skill.class)] = nil
+              end
+              if session[underscore(temp_skill.class)].present?
+                temp_skill.update_by_params!(session[underscore(temp_skill.class)])
+              end
+              yield curr_skill, temp_skill
+            end
+          end
+
           def course_skill_edit_page(user, &block)
             if params[:text_id]
               session[:text_id] = params[:text_id]
             end
-            if session[:course_text_id].blank?
+            if session[:text_id].blank?
               haml :error, :layout => true, :locals => {:error_no => ERROR_COURSE_IS_UNDECIDED}
             elsif (course = Master::Course.find_by(:text_id => session[:text_id])).nil?
               haml :error, :layout => true, :locals => {:error_no => ERROR_COURSE_NOT_EXIST}
@@ -34,6 +57,96 @@ module CxbRank
           end
         end
 
+        app.get "#{SKILL_ITEM_EDIT_URI}/?:text_id?" do
+          private_page do |user|
+            music_skill_edit_page(user) do |curr_skill, temp_skill|
+              fixed_title = "#{PAGE_TITLES[SKILL_ITEM_EDIT_URI]} [#{curr_skill.music.full_title}]"
+              add_template_paths PAGE_TEMPLATE_FILES[SKILL_ITEM_EDIT_URI]
+              haml :music_skill_edit, :layout => true, :locals => {
+                :user => user,
+                :curr_skill => curr_skill, :temp_skill => temp_skill, :fixed_title => fixed_title}
+            end
+          end
+        end
+
+        app.post SKILL_ITEM_EDIT_URI do
+          skill_klass = MusicSkill
+          session[underscore(skill_klass)] = Hash[params[underscore(skill_klass)]]
+          private_page do |user|
+            music_skill_edit_page(user) do |curr_skill, temp_skill|
+              unless temp_skill.valid?
+                haml :error, :layout => true, :locals => {
+                  :errors => temp_skill.errors,
+                  :back_uri => SiteSettings.join_site_base(request.path_info)}
+              else
+                temp_skill.calc!
+                method = (params[:update].present? ? 'put' : 'delete')
+                fixed_title = "#{PAGE_TITLES[SKILL_ITEM_EDIT_URI]} [#{curr_skill.music.full_title}]"
+                add_template_paths PAGE_TEMPLATE_FILES[SKILL_ITEM_EDIT_URI]
+                haml :music_skill_edit_conf, :layout => true, :locals => {
+                  :user => user,
+                  :curr_skill => curr_skill, :temp_skill => temp_skill, :fixed_title => fixed_title,
+                  :method => method}
+              end
+            end
+          end
+        end
+
+        app.put SKILL_ITEM_EDIT_URI do
+          private_page do |user|
+            if params[:y].present?
+              music_skill_edit_page(user) do |curr_skill, temp_skill|
+                begin
+                  temp_skill.calc!
+                  temp_skill.save!
+                  if SiteSettings.cxb_mode? or !user.point_direct
+                    skill_set = SkillSet.new(user)
+                    skill_set.load!
+                    user.point = skill_set.total_point
+                    user.point_direct = false
+                    user.point_updated_at = Time.now
+                    user.save!
+                  end
+                  clear_session_temp_skill(temp_skill)
+                  redirect SiteSettings.join_site_base(SKILL_LIST_EDIT_URI)
+                rescue
+                  haml :error, :layout => true, :locals => {
+                    :error_no => ERROR_DATABASE_SAVE_FAILED,
+                    :back_uri => SiteSettings.join_site_base(request.path_info)}
+                end
+              end
+            else
+              redirect SiteSettings.join_site_base(SKILL_ITEM_EDIT_URI)
+            end
+          end
+        end
+
+        app.delete SKILL_ITEM_EDIT_URI do
+          private_page do |user|
+            if params[:y].present?
+              music_skill_edit_page(user) do |curr_skill, temp_skill|
+                begin
+                  temp_skill.destroy
+                  skill_set = SkillSet.new(user)
+                  skill_set.load!
+                  user.point = skill_set.total_point
+                  user.point_direct = false
+                  user.point_updated_at = Time.now
+                  user.save!
+                  clear_session_temp_skill(temp_skill)
+                  redirect SiteSettings.join_site_base(SKILL_LIST_EDIT_URI)
+                rescue
+                  haml :error, :layout => true, :locals => {
+                    :error_no => ERROR_DATABASE_SAVE_FAILED,
+                    :back_uri => SiteSettings.join_site_base(request.path_info)}
+                end
+              end
+            else
+              redirect SiteSettings.join_site_base(SKILL_ITEM_EDIT_URI)
+            end
+          end
+        end
+
         app.get "#{SKILL_COURSE_ITEM_EDIT_URI}/?:text_id?" do
           private_page do |user|
             course_skill_edit_page(user) do |curr_skill, temp_skill|
@@ -52,8 +165,9 @@ module CxbRank
           private_page do |user|
             course_skill_edit_page(user) do |curr_skill, temp_skill|
               unless temp_skill.valid?
-                haml :error, :layout => true,
-                  :locals => {:errors => temp_skill.errors, :back_uri => request.path_info}
+                haml :error, :layout => true, :locals => {
+                  :errors => temp_skill.errors,
+                  :back_uri => SiteSettings.join_site_base(request.path_info)}
               else
                 temp_skill.calc!
                 method = (params[:update].present? ? 'put' : 'delete')
